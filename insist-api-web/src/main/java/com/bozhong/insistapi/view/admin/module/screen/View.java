2fb5ce2f66973498e4caafa4eb09bd13baeab06b
xiezg <1573623031@qq.com>
2017-07-21 13:45:48 +0800

    公司基础框架

---
+++ insist-api-web/src/main/java/com/bozhong/insistapi/view/admin/module/screen/View.java
@@ -0,0 +1,34 @@
+package com.bozhong.insistapi.view.admin.module.screen;
+
+import com.bozhong.common.util.StringUtil;
+import com.bozhong.insistapi.entity.AppDO;
+import com.yx.eweb.main.EWebContext;
+import com.yx.eweb.main.ScreenInter;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+
+/**
+ * Created by xiezg@317hu.com on 2017/7/21 0021.
+ */
+@Controller
+public class View implements ScreenInter {
+    @Override
+    public void excute(EWebContext eWebContext) {
+        String appId = eWebContext.getRequest().getParameter("appId");
+        eWebContext.put("menu", View.class.getSimpleName());
+        List<AppDO> appDOList = (List<AppDO>) eWebContext.getRequest().getAttribute("appDOList");
+        if (!CollectionUtils.isEmpty(appDOList) && StringUtil.isNotBlank(appId)) {
+            for (AppDO appDO : appDOList) {
+                if (appDO.getAppId().equals(appId)) {
+                    appDO.setViewSelectType(true);
+                    eWebContext.put("appDO", appDO);
+                    break;
+                }
+            }
+        }
+
+        eWebContext.getRequest().setAttribute("appDOList", appDOList);
+    }
+}
