4b746b96991ec3a1d43f4c96e4f62f1c73efa376
xiezg <1573623031@qq.com>
2017-07-21 11:30:47 +0800

    公司基础框架

--- insist-api-client/src/main/java/com/bozhong/insistapi/common/InsistApiErrorEnum.java
+++ insist-api-client/src/main/java/com/bozhong/insistapi/common/InsistApiErrorEnum.java
@@ -7,11 +7,6 @@ import com.alibaba.fastjson.JSON;
  */
 public enum InsistApiErrorEnum {
 
-    E10001("E10001", "Parameter Not Defined", "参数未定义！"),
-    E10011("E10011", "QiNiu Upload Error", "七牛上传失败"),
-    E10015("E10015", "QiNiu Get File Info Error", "七牛获取文件信息异常！"),
-    E10016("E10016", "Net Work Error", "网络连接异常！"),
-    E10017("E10017", "Open Office Service Error", "文件转换服务异常！"),
     E10018("E10018", "Login Failure Public Private Key Expire", "用户密码公钥私钥加密策略过期，请重新进入登录页面！"),;
 
 
