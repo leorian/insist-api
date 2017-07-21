1550029fb3f3ed814e79f79fe700012f3524dcc1
xiezg <1573623031@qq.com>
2017-07-21 14:26:43 +0800

    公司基础框架

--- insist-api-web/src/main/java/com/bozhong/insistapi/restful/InterfaceHttpRest.java
+++ insist-api-web/src/main/java/com/bozhong/insistapi/restful/InterfaceHttpRest.java
@@ -1,12 +1,15 @@
 package com.bozhong.insistapi.restful;
 
 import com.alibaba.fastjson.JSON;
+import com.bozhong.common.util.ResultMessageBuilder;
 import com.bozhong.insistapi.domain.InterfaceHttpDomain;
+import com.bozhong.insistapi.entity.InterfaceHttpEntity;
+import com.bozhong.insistapi.service.MongoService;
 import com.sun.jersey.spi.resource.Singleton;
 import com.yx.eweb.main.EWebRequestDTO;
 import com.yx.eweb.main.EWebServletContext;
 import org.apache.commons.beanutils.BeanUtils;
-import org.apache.commons.beanutils.BeanUtilsBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import javax.ws.rs.POST;
@@ -16,6 +19,7 @@ import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.UriInfo;
 import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -25,6 +29,9 @@ import java.util.Map;
 @Singleton
 @Path("interfaceHttp")
 public class InterfaceHttpRest {
+
+    @Autowired
+    private MongoService mongoService;
 
     @POST
     @Path("insertInterfaceHttp")
@@ -38,7 +45,17 @@ public class InterfaceHttpRest {
         } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
+
         System.out.println(JSON.toJSONString(httpDomain));
-        return "";
+
+        InterfaceHttpEntity interfaceHttpEntity = httpDomain.buildInterfaceHttpEntity();
+        interfaceHttpEntity.setInterfaceParamEntities(httpDomain.
+                buildInterfaceParamEntities(interfaceHttpEntity.getId()));
+        interfaceHttpEntity.setInterfaceResultEntities(httpDomain.
+                buildInterfaceResultEntities(interfaceHttpEntity.getId()));
+
+        mongoService.insertOne(interfaceHttpEntity);
+
+        return ResultMessageBuilder.build().toJSONString();
     }
 }
