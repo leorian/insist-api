7c6a59802a2e2a579ddd74377604966c1d21f511
xiezg <1573623031@qq.com>
2017-07-24 11:17:20 +0800

    公司基础框架

---
+++ insist-api-web/src/main/java/com/bozhong/insistapi/view/admin/module/screen/Pdf.java
@@ -0,0 +1,30 @@
+package com.bozhong.insistapi.view.admin.module.screen;
+
+import com.bozhong.common.util.StringUtil;
+import com.bozhong.insistapi.entity.AppDO;
+import com.yx.eweb.main.EWebContext;
+import com.yx.eweb.main.ScreenInter;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+
+/**
+ * Created by xiezg@317hu.com on 2017/7/24 0024.
+ */
+@Controller
+public class Pdf implements ScreenInter {
+    @Override
+    public void excute(EWebContext eWebContext) {
+        String appId = eWebContext.getRequest().getParameter("appId");
+        List<AppDO> appDOList = (List<AppDO>) eWebContext.getRequest().getAttribute("appDOList");
+        if (!CollectionUtils.isEmpty(appDOList) && StringUtil.isNotBlank(appId)) {
+            for (AppDO appDO : appDOList) {
+                if (appDO.getAppId().equals(appId)) {
+                    eWebContext.put("appDO", appDO);
+                    break;
+                }
+            }
+        }
+    }
+}
