7bd0e943eacab4c0f976fba903dd913c5d89e9ab
xiezg <1573623031@qq.com>
2017-07-25 09:18:39 +0800

    公司基础框架

--- insist-api-web/src/main/webapp/webFrame/handlebars-jquery-plugin.js
+++ insist-api-web/src/main/webapp/webFrame/handlebars-jquery-plugin.js
@@ -43,6 +43,15 @@
         }
     });
 
+    Handlebars.registerHelper("compareParamType", function (v1, v2, options) {
+        if (v1 == v2) {
+            return options.fn(this);
+        } else {
+            return options.inverse(this);
+        }
+
+    });
+
     Handlebars.registerHelper("addOne", function (index, options) {
         return parseInt(index) + 1;
     });
