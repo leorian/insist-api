bbdb2da0610b06ab236e0a9efb4f25e176202828
xiezg <1573623031@qq.com>
2017-08-24 13:42:25 +0800

    公司基础框架

--- insist-api-web/src/main/java/com/bozhong/insistapi/restful/InterfaceRpcRest.java
+++ insist-api-web/src/main/java/com/bozhong/insistapi/restful/InterfaceRpcRest.java
@@ -4,16 +4,18 @@ import com.alibaba.fastjson.JSON;
 import com.bozhong.common.util.ResultMessageBuilder;
 import com.bozhong.common.util.StringUtil;
 import com.bozhong.insistapi.domain.InterfaceRpcDomain;
-import com.bozhong.insistapi.entity.InterfaceParamEntity;
-import com.bozhong.insistapi.entity.InterfaceParamResponseEntity;
-import com.bozhong.insistapi.entity.InterfaceRpcEntity;
+import com.bozhong.insistapi.entity.*;
+import com.bozhong.insistapi.enums.OperationCategoryEnum;
+import com.bozhong.insistapi.enums.OperationTypeEnum;
 import com.bozhong.insistapi.service.MongoService;
+import com.bozhong.insistapi.task.DocHttpUtil;
 import com.sun.jersey.spi.resource.Singleton;
 import com.yx.eweb.main.EWebRequestDTO;
 import com.yx.eweb.main.EWebServletContext;
 import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -21,11 +23,10 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.UriInfo;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by xiezg@317hu.com on 2017/7/21 0021.
@@ -38,6 +39,14 @@ public class InterfaceRpcRest {
     @Autowired
     private MongoService mongoService;
 
+    /**
+     * 新增RPC接口文档
+     *
+     * @param request
+     * @param uriInfo
+     * @param httpHeaders
+     * @return
+     */
     @POST
     @Path("insertInterfaceRpc")
     public String insertInterfaceRpc(@Context Request request, @Context UriInfo uriInfo, @Context HttpHeaders httpHeaders) {
@@ -61,6 +70,17 @@ public class InterfaceRpcRest {
                 buildInterfaceResultEntities(interfaceRpcEntity.getId()));
 
         mongoService.insertOne(interfaceRpcEntity);
+
+        try {
+            //新增RPC接口文档记录操作日志
+            InsistApiOperationEntity insistApiOperationEntity = new InsistApiOperationEntity();
+            insistApiOperationEntity.buildOperationEntity(interfaceRpcEntity.getAppId(),
+                    getAppName(interfaceRpcEntity.getAppId()), OperationTypeEnum.ADD.name()
+                    , OperationCategoryEnum.RPC.name(), interfaceRpcEntity);
+            mongoService.insertOne(insistApiOperationEntity);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return ResultMessageBuilder.build().toJSONString();
     }
@@ -151,6 +171,17 @@ public class InterfaceRpcRest {
 
         mongoService.updateOneByKey(interfaceId, interfaceRpcEntity);
 
+        try {
+            //更新RPC接口文档记录操作日志
+            InsistApiOperationEntity insistApiOperationEntity = new InsistApiOperationEntity();
+            insistApiOperationEntity.buildOperationEntity(interfaceRpcEntity.getAppId()
+                    , getAppName(interfaceRpcEntity.getAppId()), OperationTypeEnum.UPDATE.name(),
+                    OperationCategoryEnum.RPC.name(), interfaceRpcEntity);
+            mongoService.insertOne(insistApiOperationEntity);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return ResultMessageBuilder.build().toJSONString();
     }
 
@@ -159,7 +190,34 @@ public class InterfaceRpcRest {
     public String deleteInterfaceRpc(@Context Request request, @Context UriInfo uriInfo, @Context HttpHeaders httpHeaders) {
         Map<String, Object> param = ((EWebRequestDTO) EWebServletContext.getEWebContext().getParam()).getRequestParam();
         String interfaceId = (String) param.get("interfaceId");
+        try {
+            //删除RPC接口记录操作日志
+            InterfaceRpcEntity interfaceRpcEntity = mongoService.getOneByInterfaceId(interfaceId, InterfaceRpcEntity.class);
+            InsistApiOperationEntity insistApiOperationEntity = new InsistApiOperationEntity();
+            insistApiOperationEntity.buildOperationEntity(interfaceRpcEntity.getAppId(), getAppName(interfaceRpcEntity.getAppId())
+                    , OperationTypeEnum.DELETE.name(), OperationCategoryEnum.RPC.name(), interfaceRpcEntity);
+            mongoService.insertOne(insistApiOperationEntity);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         mongoService.deleteOneByKey(interfaceId, InterfaceRpcEntity.class);
         return ResultMessageBuilder.build().toJSONString();
+    }
+
+    /**
+     * 获取哦应用名称
+     *
+     * @param appId
+     * @return
+     */
+    private String getAppName(String appId) throws IOException {
+        List<AppDO> appDOList = DocHttpUtil.getAllAppDOList();
+        Map<String, String> map = new HashMap<>();
+        if (!CollectionUtils.isEmpty(appDOList)) {
+            for (AppDO appDO : appDOList) {
+                map.put(appDO.getAppId(), appDO.getAppName());
+            }
+        }
+        return map.get(appId);
     }
 }
