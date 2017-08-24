cc6b8f0da21a973c71d3f14ab67b469484641404
xiezg <1573623031@qq.com>
2017-08-24 15:57:05 +0800

    公司基础框架

--- insist-api-web/src/main/java/com/bozhong/insistapi/view/admin/module/screen/History.java
+++ insist-api-web/src/main/java/com/bozhong/insistapi/view/admin/module/screen/History.java
@@ -1,8 +1,11 @@
 package com.bozhong.insistapi.view.admin.module.screen;
 
+import com.bozhong.insistapi.task.DocHttpUtil;
 import com.yx.eweb.main.EWebContext;
 import com.yx.eweb.main.ScreenInter;
 import org.springframework.stereotype.Controller;
+
+import java.io.IOException;
 
 /**
  * Created by xiezg@317hu.com on 2017/8/24 0024.
@@ -12,5 +15,11 @@ public class History implements ScreenInter {
     @Override
     public void excute(EWebContext eWebContext) {
         eWebContext.put("menu", History.class.getSimpleName());
+
+        try {
+            eWebContext.put("allAppList", DocHttpUtil.getAllAppDOList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
